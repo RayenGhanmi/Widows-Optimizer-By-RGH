83bf3901b1b17d3e33470f6cfabeb8b51c54bd7a
RayenGhanmi <108760398+RayenGhanmi@users.noreply.github.com>
2022-07-10 15:28:13 +0100

    Delete Data directory

--- Data/14.cmd
+++
@@ -1,8 +0,0 @@
-wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Before optimizing", 100, 7
-DISM /Online /Cleanup-Image /CheckHealth
-DISM /Online /Cleanup-Image /ScanHealth
-DISM /Online /Cleanup-Image /RestoreHealth
-sfc/scannow
-cd C:\ProgramData\Microsoft\Windows Defender\Platform\4.18*
-MpCmdRun -Scan -ScanType 2
-MpCmdRun -Scan -ScanType -BootSectorScan
